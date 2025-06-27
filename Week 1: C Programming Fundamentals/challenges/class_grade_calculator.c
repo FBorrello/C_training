fb1aa2946c61fa6d7e6d1281aabfcd5fa2a3f7fb
Fabrizio Borrello <borrellofabrizio@gmail.com>
2025-06-27 09:34:17 +0200

    Add Accept the number of students (via scanf)

--- Week 1: C Programming Fundamentals/challenges/class_grade_calculator.c
+++ Week 1: C Programming Fundamentals/challenges/class_grade_calculator.c
@@ -5,13 +5,20 @@ Use a switch or if statements for grade assignment.
 Store names in character arrays (e.g., char name[50]).
 Commit your code to your Git repo with clear comments and a README*/
 
+#include <stdio.h>
+
 int main ()
 {
     /*Accept the number of students (via scanf).*/
+    int studentsNumber;
+    printf("Enter number of students: ");
+    scanf("%d", &studentsNumber);
+    printf("The number of strudent is %d\n", studentsNumber);
+
     /*For each student, input their name (as a string) and three test scores (integers between 0 and 100).*/
     /*Calculate the average score for each student and assign a letter grade (A: 90–100, B: 80–89, C: 70–79, D: 60–69, F: <60).*/
     /*Print a formatted report with each student’s name, average score, and letter grade.*/
     /*Handle errors: reject invalid scores (outside 0–100) and ensure the number of students is positive.*/
-    
+
     return 0;
 }
